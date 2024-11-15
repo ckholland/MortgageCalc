e14db698963e50fe953edc5677d21261729fced9
ckholland <172523543+ckholland@users.noreply.github.com>
2024-11-15 00:32:03 -0500

    Fixing default values and adding params to final equation.

--- calc.py
+++ calc.py
@@ -35,28 +35,12 @@ def mortgage_calculator(loan_amount, interest_rate, loan_term, annual_property_t
 
 
 
-loan_amount = st.number_input("Enter loan amount:", value = "loan amount")
-interest_rate = st.number_input("Enter rate:", value = "rate %")
-loan_term = st.number_input("Term:", value = "years")
+loan_amount = st.number_input("Enter loan amount:", value = 300000)
+interest_rate = st.number_input("Enter rate:", value = .0415)
+loan_term = st.number_input("Term:", value = 30)
 annual_property_tax=0
 annual_home_insurance=0
 pmi_rate=0
 down_payment=0
 
-payment = mortgage_calculator(loan_amount,interest_rate,loan_term)
-
-"""
-    Calculates the monthly mortgage payment, including principal & interest, property tax, home insurance, and PMI.
-
-    Args:
-        loan_amount (float): The total loan amount.
-        interest_rate (float): The annual interest rate (as a decimal).
-        loan_term (int): The loan term in years.
-        annual_property_tax (float): The annual property tax amount.
-        annual_home_insurance (float): The annual home insurance amount.
-        pmi_rate (float, optional): The annual PMI rate (as a decimal). Defaults to 0.005.
-        down_payment (float, optional): The down payment amount. Defaults to 0.
-
-    Returns:
-        float: The monthly mortgage payment.
-"""
+payment = mortgage_calculator(loan_amount,interest_rate,loan_term, annual_property_tax, annual_home_insurance, pmi_rate, down_payment)
